aef79369233c20ed30f54f57ad31ccc579402a4a
jipenaflor <rome.img23@gmail.com>
2024-12-15 11:01:21 +0800

    Connected wikimedia stream to local kafka

---
+++ src/main/java/org/jipenaflor/kafka/wikimedia/WikimediaChangesProducer.java
@@ -0,0 +1,41 @@
+package org.jipenaflor.kafka.wikimedia;
+
+import com.launchdarkly.eventsource.EventHandler;
+import com.launchdarkly.eventsource.EventSource;
+import org.apache.kafka.clients.producer.KafkaProducer;
+import org.apache.kafka.clients.producer.ProducerConfig;
+import org.apache.kafka.common.serialization.StringSerializer;
+
+import java.net.URI;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
+
+public class WikimediaChangesProducer {
+
+    public static void main(String[] args) throws InterruptedException {
+        String bootstrapServers = "172.18.161.170:9092";
+
+        Properties properties = new Properties();
+        properties.setProperty(ProducerConfig.BOOTSTRAP_SERVERS_CONFIG, bootstrapServers);
+        properties.setProperty(ProducerConfig.KEY_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+        properties.setProperty(ProducerConfig.VALUE_SERIALIZER_CLASS_CONFIG, StringSerializer.class.getName());
+
+        KafkaProducer<String, String> producer = new KafkaProducer<>(properties);
+
+        String topic = "wikimedia.recentchange";
+
+        // handle the events from the streams and send it to the producer
+        EventHandler eventHandler = new WikimediaChangeHandler(producer, topic);
+
+        String url = "https://stream.wikimedia.org/v2/stream/recentchange";
+
+        EventSource.Builder builder = new EventSource.Builder(eventHandler, URI.create(url));
+        EventSource eventSource = builder.build();
+
+        // start the producer in another thread
+        eventSource.start();
+
+        // produce for 3 minutes then block the program
+        TimeUnit.MINUTES.sleep(3);
+    }
+}
